c13fed505056d651182eacbd702efde96e33961d
shubhammarathe14 <130459005+shubhammarathe14@users.noreply.github.com>
2025-02-19 13:04:36 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,24 +5,25 @@ echo "Starting deployment..."
 
 # Ensure /var/www/angular-app exists
 sudo mkdir -p /var/www/angular-app
-sudo chown -R $USER:$USER /var/www/angular-app
+sudo chown -R ubuntu:ubuntu /var/www/angular-app
 
 # Navigate to the deployment directory
 cd /var/www/angular-app || exit
 
-# Install dependencies using full path
+# Install dependencies
 echo "Installing dependencies..."
-/usr/bin/npm install --legacy-peer-deps
+npm install --legacy-peer-deps
 
 # Build the Angular application
 echo "Building Angular application..."
-/usr/bin/ng build --configuration=production
+npx ng build --configuration=production
 
 # Serve the Angular app
 echo "Starting Angular application..."
 nohup npx http-server -p 80 dist/ecommerce/ &
 
 echo "Deployment complete!"
+
 
 
 
